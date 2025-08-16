a769c696d2a256af3324dfa9a3b3a4751f7d2a68
SpringDev-ai <saraswathim0803@gmail.com>
2025-08-16 18:01:42 +0530

    User Entity class is updated with fields

--- src/main/java/com/jspiders/User_App/Entity/User.java
+++ src/main/java/com/jspiders/User_App/Entity/User.java
@@ -1,5 +1,21 @@
 package com.jspiders.User_App.Entity;
 
-public class User {
+import org.springframework.data.annotation.Id;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+
+public class User
+{
+@Id 
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int userId;
+private String userName;
+private String userEmail;
+private String userPassword;
+private String userContact;
+private String address;
 }
